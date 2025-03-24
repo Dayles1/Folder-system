92daa35041873058cb443047c3536871e72330d9
Dayles1 <sololeveling0025@gmail.com>
2025-03-24 21:43:31 +0500

    i wrote Full worked Store method

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\FolderStoreRequest;
 
 class FolderController extends Controller
 {
-    public function store()
+    public function store(FolderStoreRequest $request)
     {
-        
+        $icon=$this->uploadPhoto($request->file('icon'),'icons');
+
+        $folder=Folder::create([
+            'name'=>$request->name,
+            'icon'=>$icon,
+            'parent_id'=>$request->parent_id,
+        ]);
+        return $this->success($folder,'Folder created successfully',201);
     }
+    
 }
