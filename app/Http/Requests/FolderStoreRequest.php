b508680b411fa578b8e9cab5075192135a35194c
Dayles1 <sololeveling0025@gmail.com>
2025-03-24 21:33:42 +0500

    +validate for store folder

--- app/Http/Requests/FolderStoreRequest.php
+++ app/Http/Requests/FolderStoreRequest.php
@@ -11,7 +11,7 @@ class FolderStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class FolderStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=>'required|string|max:30',
+            'icon'=>'required|image',
+            'parent_id'=>'nullable|exists:folders,id'
         ];
     }
 }
