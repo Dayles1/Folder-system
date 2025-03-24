0408df510e618bed0062ccd04686f4a10e38442c
Dayles1 <sololeveling0025@gmail.com>
2025-03-24 21:52:58 +0500

    +relation

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    public function parent()
+    {
+        return $this->belongsTo(Folder::class, 'parent_id');  // Tegishli ota kategoriya
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Folder::class, 'parent_id');  // Bolalar kategoriyasi
+    }
 }
